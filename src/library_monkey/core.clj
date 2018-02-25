4df4cc7a7876b0a0842691e2b9e99ea06c7e3dc3
Charles Fourdrignier <charles.fourdrignier@gmail.com>
2018-02-25 21:11:55 +0100

    Reindent files with cljfmt

--- src/library_monkey/core.clj
+++ src/library_monkey/core.clj
@@ -1,13 +1,12 @@
 (ns library-monkey.core
-    (:gen-class)
-    (:require [library-monkey.network :as net]
-      [library-monkey.html-parse :as parse]))
-  
-  (defn -main
-    [user password & args]
-    (->> 
-      (net/auth-cookie user password)
-      (net/get-borrowings)
-      (parse/extract-borrowings)
-      (#(doseq [b %] (println (:titre b) (:date-de-retour b))))))
-  
+  (:gen-class)
+  (:require [library-monkey.network :as net]
+            [library-monkey.html-parse :as parse]))
+
+(defn -main
+  [user password & args]
+  (->>
+   (net/auth-cookie user password)
+   (net/get-borrowings)
+   (parse/extract-borrowings)
+   (#(doseq [b %] (println (:titre b) (:date-de-retour b))))))
