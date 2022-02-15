e31865ea5015d0ab812a06f4b64e8a1e5b1820e9
Charles Fourdrignier <charles.fourdrignier@gmail.com>
2022-02-15 12:27:52 +0100

    Datascript - Chargement de tous les utilisateurs

--- repl/introduce_datascript.clj
+++ repl/introduce_datascript.clj
@@ -3,21 +3,31 @@
 
 (def configuration (clojure.edn/read-string (slurp "config.edn")))
 
-(def schema {:user {:db/cardinality :db.cardinality/one}})
+(def schema {:username {:db/unique :db.unique/identity}
+             :user {:db/valueType :db.type/ref}})
 (def conn (d/create-conn schema))
 
-(let [library lm/amiens-library
-      user (get-in configuration [:accounts 0])
-      cookie (authenticate library
-                           (:username user)
-                           (:password user))]
-  (d/transact! conn
-               (map
-                (fn [borrowing] (assoc borrowing :user user))
-                (get-borrowings library cookie))))
+(let [library lm/amiens-library]
+  (doseq [user (:accounts configuration)]
+    (d/transact! conn [user])
+    (let [cookie (authenticate library
+                               (:username user)
+                               (:password user))]
+      (d/transact! conn
+                   (map
+                    (fn [borrowing] (assoc borrowing :user user))
+                    (get-borrowings library cookie))))))
 
+(def query-utilisateurs-avec-emprunt
+  '[:find ?pseudo
+    :where
+    [?e :user ?u]
+    [?u :pseudo ?pseudo]])
 
-(d/q '[:find ?title ?date
-       :where
-       [?e :titre ?title]
-       [?e :date-de-retour ?date]] @conn)
+(def query-tous-utilisateurs
+  '[:find ?pseudo
+    :where
+    [?u :pseudo ?pseudo]])
+
+(d/q query-utilisateurs-avec-emprunt @conn)
+(d/q query-tous-utilisateurs @conn)
